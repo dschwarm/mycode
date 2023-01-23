5cf1b9b1249be802e805284264b627f467005509
dschwarm <david.schwarm@gmail.com>
2023-01-23 16:55:01 +0000

    Learnung about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("David Given Schwarm")
+    fmt.Println("Irvine California")
+}
---
+++ ready-to-learn-about.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func main() {
+    fmt.Println("Looks like you're ready to learn GoLnang with Alta3 Research")
+    fmt.Println("My favorite number is 43")
+    fmt.Println("However, i grab random numbers as well", rand.Intn(10))
+}
+
